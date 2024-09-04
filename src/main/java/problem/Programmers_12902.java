0d1580fdf97421aa61477f1d4d963d7a9706bbc0
Baek Seung Yeop <tmdduq1999@naver.com>
2024-09-04 18:00:58 +0900

    주석 추가

--- src/main/java/problem/Programmers_12902.java
+++ src/main/java/problem/Programmers_12902.java
@@ -5,11 +5,16 @@ public class Programmers_12902 {
     public static long solution(int n) {
         if (n % 2 == 1) return 0;
         long[] dp = new long[n + 1];
+
+        // 초기값 설정
         dp[0] = 1;
         dp[2] = 3;
 
         for (int i = 4; i <= n; i += 2) {
+            // 아전 경우의 수 * 늘어난 칸에 대한 경우의 수 (3)
             dp[i] = dp[i - 2] * 3;
+
+            // 노션 참고
             for (int j = i - 4; j >= 0; j -= 2) {
                 dp[i] += dp[j] * 2;
             }
