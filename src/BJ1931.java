410d2325f23507984776aed3b614b6f34379289c
iqveou6 <swgm0719@naver.com>
2024-09-04 09:19:07 +0900

    BJ 1931

--- src/BJ1931.java
+++ src/BJ1931.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Bj1931 {
+public class BJ1931 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
