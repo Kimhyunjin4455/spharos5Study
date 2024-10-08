90aaf3ab1578d4e4e8a82fc35f211965f28beb29
tlswl7479 <tlswl7479@gmail.com>
2024-10-08 17:16:24 +0900

    BJ_2559, BBJ_15988

---
+++ src/algorithm/BJ_15988.java
@@ -0,0 +1,25 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_15988 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long[] dp = new long[1000001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 1000000; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
+        }
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
---
+++ src/algorithm/BJ_2559.java
@@ -0,0 +1,40 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class BJ_2559 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 첫 K일 동안
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        int maxSum = sum;
+
+        // 슬라이딩 윈도우
+        for (int i = k; i < n; i++) {
+            sum = sum - arr[i - k] + arr[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        System.out.println(maxSum);
+    }
+}
+
